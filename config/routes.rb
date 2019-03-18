800f1dd552d475dd9403efcb935b25868e39bfed
KazimieraLuce <kazimiera.luce@aya.yale.edu>
2019-03-18 19:50:43 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'endorefs#index'
   resources :articles, only: [:index, :show, :create, :new] do
-    resources :titles#, only: [:index, :show]
-    resources :urls#, only: [:index, :show]
-    resources :descriptions#, only: [:index, :show]
+    resources :titles, only: [:index, :show]
+    resources :urls, only: [:index, :show]
+    resources :descriptions, only: [:index, :show]
   end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
